fa65bbf48bd65e7d4f9e7d845fb43f131aac889c
danp <danp@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-07-24 12:14:41 +0000

    after TER upload
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/crawler/trunk@10713 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "crawler"
 #
-# Auto generated 23-07-2008 21:25
+# Auto generated 24-07-2008 14:03
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Libraries and scripts for crawling the TYPO3 page tree. Used for re-caching, re-indexing, publishing applications etc.',
 	'category' => 'module',
 	'shy' => 0,
-	'version' => '2.0.0',
+	'version' => '2.1.0',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -42,7 +42,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:14:{s:24:"class.tx_crawler_lib.php";s:4:"dc2f";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"132e";s:14:"ext_tables.php";s:4:"35c1";s:14:"ext_tables.sql";s:4:"6370";s:16:"locallang_db.php";s:4:"ca54";s:12:"cli/conf.php";s:4:"a5ed";s:19:"cli/crawler_cli.php";s:4:"46a7";s:21:"cli/crawler_cli.phpsh";s:4:"8462";s:18:"cli/crawler_im.php";s:4:"e97b";s:12:"doc/TODO.txt";s:4:"aaa6";s:14:"doc/manual.sxw";s:4:"57e1";s:38:"modfunc1/class.tx_crawler_modfunc1.php";s:4:"a9ef";s:22:"modfunc1/locallang.php";s:4:"6652";}',
+	'_md5_values_when_last_written' => 'a:15:{s:24:"class.tx_crawler_lib.php";s:4:"85ce";s:21:"ext_conf_template.txt";s:4:"f1cc";s:12:"ext_icon.gif";s:4:"a434";s:17:"ext_localconf.php";s:4:"132e";s:14:"ext_tables.php";s:4:"35c1";s:14:"ext_tables.sql";s:4:"6370";s:16:"locallang_db.php";s:4:"ca54";s:12:"cli/conf.php";s:4:"a5ed";s:19:"cli/crawler_cli.php";s:4:"46a7";s:21:"cli/crawler_cli.phpsh";s:4:"8462";s:18:"cli/crawler_im.php";s:4:"e97b";s:12:"doc/TODO.txt";s:4:"aaa6";s:14:"doc/manual.sxw";s:4:"57e1";s:38:"modfunc1/class.tx_crawler_modfunc1.php";s:4:"a9ef";s:22:"modfunc1/locallang.php";s:4:"6652";}',
 	'suggests' => array(
 	),
 );
