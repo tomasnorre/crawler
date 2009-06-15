c75e7d9952ab4bba8c993fd4a758d9931af17da3
fabriziobranca <fabriziobranca@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-06-15 15:44:30 +0000

    made getTotalPagesCount() public
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/crawler/branches/2009-queue-refactoring@21434 735d13b6-9817-0410-8766-e36946ffe9aa

--- view/class.tx_crawler_view_pagination.php
+++ view/class.tx_crawler_view_pagination.php
@@ -1,31 +1,31 @@
 <?php
 class tx_crawler_view_pagination{
 	protected $template = 'EXT:crawler/template/pagination.php';
-	
-	
+
+
 	/**
 	 * @var int $perpage number of items perPage
 	 */
 	protected $perPage;
-	
+
 	/**
-	 * @var int $currentOffset current offset 
+	 * @var int $currentOffset current offset
 	 */
 	protected $currentOffset;
-	
+
 	/**
 	 * @var int $totalItemCount number of total item
 	 */
 	protected $totalItemCount;
-	
+
 	/**
 	 * @var string $baseUrl
 	 */
 	protected $baseUrl;
 
 
-	
-	
+
+
 	/**
 	 * Method to render the view.
 	 *
@@ -40,7 +40,7 @@ class tx_crawler_view_pagination{
 
 		return $content;
 	}
-	
+
 	/**
 	 * Returns the currently configured offset-
 	 * @return int
@@ -48,34 +48,34 @@ class tx_crawler_view_pagination{
 	public function getCurrentOffset() {
 		return $this->currentOffset;
 	}
-	
+
 	/**
 	 * Method to read the number of items per page
-	 * 
+	 *
 	 * @return int
 	 */
 	public function getPerPage() {
 		return $this->perPage;
 	}
-	
+
 	/**
 	 * Method to set the current offset from start
-	 * 
+	 *
 	 * @param int $currentOffset
 	 */
 	public function setCurrentOffset($currentOffset) {
 		$this->currentOffset = $currentOffset;
 	}
-	
+
 	/**
 	 * Number of items per page.
-	 * 
+	 *
 	 * @param int $perPage
 	 */
 	public function setPerPage($perPage) {
 		$this->perPage = $perPage;
 	}
-	
+
 	/**
 	 * returns the total number of items
 	 * @return int
@@ -83,28 +83,28 @@ class tx_crawler_view_pagination{
 	public function getTotalItemCount() {
 		return $this->totalItemCount;
 	}
-	
+
 	/**
 	 * Method to set the total number of items in the pagination
-	 * 
+	 *
 	 * @param int $totalItemCount
 	 */
 	public function setTotalItemCount($totalItemCount) {
 		$this->totalItemCount = $totalItemCount;
 	}
-	
+
 	/**
 	 * Returns the total number of pages needed to  display all content which
 	 * is paginatable
 	 *
 	 * @return int
 	 */
-	protected function getTotalPagesCount(){
+	public function getTotalPagesCount(){
 	 	return ceil($this->getTotalItemCount() / $this->getPerPage());
 	}
-	
+
 	/**
-	 * This method is used to caluclate the label for a pageoffset, 
+	 * This method is used to caluclate the label for a pageoffset,
 	 * in normal cases its the internal offset + 1
 	 *
 	 * @param int $pageoffset
@@ -113,7 +113,7 @@ class tx_crawler_view_pagination{
 	protected function getLabelForPageOffset($pageoffset){
 		return $pageoffset + 1;
 	}
-	
 
-}	
+
+}
 ?>
