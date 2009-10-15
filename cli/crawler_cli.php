896a4c6010803114766d4469b7d8dc378f85405d
danp <danp@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-10-15 15:01:39 +0000

    add exit status
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/crawler/trunk@25414 735d13b6-9817-0410-8766-e36946ffe9aa

--- cli/crawler_cli.php
+++ cli/crawler_cli.php
@@ -4,6 +4,16 @@ if (!defined('TYPO3_cliMode'))	die('You cannot run this script directly!');
 require_once(t3lib_extMgm::extPath('crawler').'class.tx_crawler_lib.php');
 
 $crawlerObj = t3lib_div::makeInstance('tx_crawler_lib');
-$crawlerObj->CLI_main();
+$result= $crawlerObj->CLI_main();
+
+if ($result===false) {
+	exit(-1);	//unknown erroor
+}
+else if ($result ==0) {
+	exit(1);	//success and finishes (no items remaining)
+}
+else {
+	exit(0);	//sucess and items remaining
+}
 
 ?>
