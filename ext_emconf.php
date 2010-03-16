d71bad5f2f368d7b103c438488fbcf7f48b5b9bb
ohader <ohader@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-03-16 17:07:50 +0000

    Updated ext_emconf.php and raised version to 3.0.6
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/crawler/trunk@31161 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,13 +1,13 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: "crawler"
+# Extension Manager/Repository config file for ext "crawler".
 #
-# Auto generated 02-04-2009 12:25
+# Auto generated 16-03-2010 18:07
 #
 # Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Libraries and scripts for crawling the TYPO3 page tree. Used for re-caching, re-indexing, publishing applications etc.',
 	'category' => 'module',
 	'shy' => 0,
-	'version' => '3.0.5',
+	'version' => '3.0.6',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -40,10 +40,10 @@ $EM_CONF[$_EXTKEY] = array(
 		'conflicts' => array(
 		),
 		'suggests' => array(
-			'realurl' => ''
+			'realurl' => '',
 		),
 	),
-	'_md5_values_when_last_written' => 'a:15:{s:24:"class.tx_crawler_lib.php";s:4:"da30";s:21:"ext_conf_template.txt";s:4:"c934";s:12:"ext_icon.gif";s:4:"a434";s:17:"ext_localconf.php";s:4:"132e";s:14:"ext_tables.php";s:4:"35c1";s:14:"ext_tables.sql";s:4:"35b7";s:16:"locallang_db.php";s:4:"ca54";s:12:"cli/conf.php";s:4:"a5ed";s:19:"cli/crawler_cli.php";s:4:"46a7";s:21:"cli/crawler_cli.phpsh";s:4:"8462";s:18:"cli/crawler_im.php";s:4:"e97b";s:12:"doc/TODO.txt";s:4:"aaa6";s:14:"doc/manual.sxw";s:4:"57e1";s:38:"modfunc1/class.tx_crawler_modfunc1.php";s:4:"a9ef";s:22:"modfunc1/locallang.php";s:4:"6652";}',
+	'_md5_values_when_last_written' => 'a:67:{s:24:"class.tx_crawler_api.php";s:4:"0e97";s:25:"class.tx_crawler_auth.php";s:4:"8b90";s:32:"class.tx_crawler_contextMenu.php";s:4:"0aa7";s:24:"class.tx_crawler_lib.php";s:4:"5610";s:28:"class.tx_crawler_tcaFunc.php";s:4:"0fce";s:16:"ext_autoload.php";s:4:"f7a6";s:21:"ext_conf_template.txt";s:4:"7352";s:12:"ext_icon.gif";s:4:"a434";s:17:"ext_localconf.php";s:4:"74bb";s:14:"ext_tables.php";s:4:"f256";s:14:"ext_tables.sql";s:4:"7794";s:33:"icon_tx_crawler_configuration.gif";s:4:"7a69";s:16:"locallang_db.xml";s:4:"86f8";s:7:"tca.php";s:4:"0116";s:17:"cli/bootstrap.php";s:4:"6596";s:28:"cli/class.tx_crawler_cli.php";s:4:"cce2";s:34:"cli/class.tx_crawler_cli_flush.php";s:4:"eb2a";s:31:"cli/class.tx_crawler_cli_im.php";s:4:"ea63";s:12:"cli/conf.php";s:4:"7d9f";s:19:"cli/crawler_cli.php";s:4:"5c75";s:21:"cli/crawler_cli.phpsh";s:4:"e110";s:21:"cli/crawler_flush.php";s:4:"19e6";s:18:"cli/crawler_im.php";s:4:"dfa7";s:12:"doc/TODO.txt";s:4:"dac8";s:14:"doc/manual.sxw";s:4:"b62d";s:59:"domain/events/class.tx_crawler_domain_events_dispatcher.php";s:4:"fb38";s:61:"domain/events/interface.tx_crawler_domain_events_observer.php";s:4:"ce0d";s:60:"domain/lib/class.tx_crawler_domain_lib_abstract_dbobject.php";s:4:"dcb3";s:62:"domain/lib/class.tx_crawler_domain_lib_abstract_repository.php";s:4:"acf7";s:50:"domain/process/class.tx_crawler_domain_process.php";s:4:"deee";s:61:"domain/process/class.tx_crawler_domain_process_collection.php";s:4:"597c";s:61:"domain/process/class.tx_crawler_domain_process_repository.php";s:4:"10fd";s:52:"domain/queue/class.tx_crawler_domain_queue_entry.php";s:4:"5135";s:57:"domain/queue/class.tx_crawler_domain_queue_repository.php";s:4:"ca34";s:48:"domain/reason/class.tx_crawler_domain_reason.php";s:4:"31d3";s:37:"hooks/class.tx_crawler_hooks_tsfe.php";s:4:"1d62";s:38:"modfunc1/class.tx_crawler_modfunc1.php";s:4:"4985";s:22:"modfunc1/locallang.xml";s:4:"d478";s:46:"scheduler/class.tx_crawler_scheduler_crawl.php";s:4:"d1e2";s:69:"scheduler/class.tx_crawler_scheduler_crawlAdditionalFieldProvider.php";s:4:"c177";s:46:"scheduler/class.tx_crawler_scheduler_flush.php";s:4:"cdf4";s:69:"scheduler/class.tx_crawler_scheduler_flushAdditionalFieldProvider.php";s:4:"8c1a";s:43:"scheduler/class.tx_crawler_scheduler_im.php";s:4:"f4e8";s:66:"scheduler/class.tx_crawler_scheduler_imAdditionalFieldProvider.php";s:4:"a8b5";s:44:"system/class.tx_crawler_system_validator.php";s:4:"ebdd";s:23:"template/pagination.php";s:4:"a519";s:16:"template/res.css";s:4:"e202";s:25:"template/process/list.php";s:4:"9ece";s:32:"template/process/res/img/add.png";s:4:"1988";s:37:"template/process/res/img/arrow_in.png";s:4:"d6c2";s:38:"template/process/res/img/arrow_out.png";s:4:"578e";s:42:"template/process/res/img/arrow_refresh.png";s:4:"6b95";s:41:"template/process/res/img/bullet_green.png";s:4:"0aa5";s:42:"template/process/res/img/bullet_orange.png";s:4:"5cfa";s:39:"template/process/res/img/bullet_red.png";s:4:"d24b";s:42:"template/process/res/img/control_pause.png";s:4:"9c20";s:41:"template/process/res/img/control_play.png";s:4:"3170";s:41:"template/process/res/img/control_stop.png";s:4:"edf6";s:46:"template/process/res/img/control_stop_blue.png";s:4:"2aee";s:33:"template/process/res/img/stop.png";s:4:"1488";s:39:"tests/class.tx_crawler_api_testcase.php";s:4:"aa6b";s:59:"tests/data/canCreateQueueEntrysUsingConfigurationRecord.xml";s:4:"bc3d";s:45:"tests/data/canNotAddDuplicatePagesToQueue.xml";s:4:"837e";s:67:"tests/events/class.tx_crawler_domain_events_dispatcher_testcase.php";s:4:"a361";s:57:"tests/events/data/class.tx_crawler_domain_events_test.php";s:4:"3046";s:41:"view/class.tx_crawler_view_pagination.php";s:4:"30e6";s:51:"view/process/class.tx_crawler_view_process_list.php";s:4:"0a02";}',
 	'suggests' => array(
 	),
 );
