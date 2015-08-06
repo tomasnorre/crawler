cc5b2aa43599e8c15b7d35598a818acbf3fa8188
Tomas Norre Mikkelsen <tomasnorre@gmail.com>
2015-08-06 10:15:13 +0200

    [BUGFIX] PHP Dependency
    Set PHP dependency to 5.3.0-5.6.99
    
    Change-Id: I867a162580b46126c95402947e38514984a234fb
    Resolves: #68736
    Releases: TYPO3_6-2
    Reviewed-on: http://review.typo3.org/42302
    Reviewed-by: Stefan Rotsch <stefan.rotsch@aoemedia.de>
    Tested-by: Stefan Rotsch <stefan.rotsch@aoemedia.de>

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'php' => '5.3.0-5.5.0',
+			'php' => '5.3.0-5.6.99',
 			'typo3' => '4.5.0-6.2.99',
 		),
 		'conflicts' => array(
