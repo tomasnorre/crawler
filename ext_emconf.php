bfd35e901f6c7089d2f415d02ce702de0f992200
Markus Kasten <markus@work.de>
2015-10-07 15:28:11 +0200

    [FIX] Fix TYPO3 version in emconf.

--- ext_emconf.php
+++ ext_emconf.php
@@ -33,7 +33,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.3.0-5.6.99',
-			'typo3' => '4.5.0-7.2.99',
+			'typo3' => '6.2.0-7.99.99',
 		),
 		'conflicts' => array(
 		),
