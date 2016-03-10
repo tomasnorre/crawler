6eebd0e63681115e6ab50ca0ce92b5ab499a5b1b
Tomas Norre Mikkelsen <tomas.mikkelsen@aoe.com>
2016-03-10 15:28:09 +0100

    [TASK] config.absRefPrefix not respected by tx_crawler_lib::getFrontendBasePath()

--- Tests/Unit/class.tx_crawler_lib_testcase.php
+++ Tests/Unit/class.tx_crawler_lib_testcase.php
@@ -33,16 +33,6 @@
 class tx_crawler_lib_testcase extends \TYPO3\CMS\Core\Tests\UnitTestCase {
 
 	/**
-	 * @var array
-	 */
-	protected $coreExtensionsToLoad = array('cms');
-
-	/**
-	 * @var array
-	 */
-	protected $testExtensionsToLoad = array('typo3conf/ext/crawler');
-
-	/**
 	 * @var \tx_crawler_lib
 	 */
 	protected $crawlerLibrary;
@@ -53,7 +43,8 @@ class tx_crawler_lib_testcase extends \TYPO3\CMS\Core\Tests\UnitTestCase {
 	* @return void
 	*/
 	public function setUp() {
-		$this->crawlerLibrary = $this->getMock('\tx_crawler_lib', array('buildRequestHeaderArray', 'executeShellCommand'));
+		$this->crawlerLibrary = $this->getMock('\tx_crawler_lib', array('buildRequestHeaderArray', 'executeShellCommand'), array(), '', FALSE);
+		$GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['crawler'] = 'a:20:{s:9:"sleepTime";s:4:"1000";s:16:"sleepAfterFinish";s:2:"10";s:11:"countInARun";s:2:"20";s:14:"purgeQueueDays";s:2:"14";s:12:"processLimit";s:1:"9";s:17:"processMaxRunTime";s:3:"300";s:14:"maxCompileUrls";s:5:"10000";s:12:"processDebug";s:1:"0";s:14:"processVerbose";s:1:"0";s:16:"crawlHiddenPages";s:1:"1";s:7:"phpPath";s:16:"/usr/bin/php5 -q";s:14:"enableTimeslot";s:1:"1";s:11:"logFileName";s:0:"";s:9:"follow30x";s:1:"0";s:18:"makeDirectRequests";s:1:"1";s:16:"frontendBasePath";s:1:"/";s:22:"cleanUpOldQueueEntries";s:1:"1";s:19:"cleanUpProcessedAge";s:1:"2";s:19:"cleanUpScheduledAge";s:1:"7";s:21:"PageUidRootTypoScript";s:1:"1";}';
 	}
 
 	/**
@@ -135,34 +126,5 @@ class tx_crawler_lib_testcase extends \TYPO3\CMS\Core\Tests\UnitTestCase {
 
 		$this->assertEquals($testHeader . str_repeat("\r\n", 2), $result['request']);
 		$this->assertEquals($testContent, $result['content']);
-	}
-
-	/**
-	 * @test
-	 */
-	public function canReadHttpResponseFromStream() {
-		require_once __DIR__ . '/proxies/class.tx_crawler_lib_proxy.php';
-
-		$dummyContent = 'Lorem ipsum';
-		$dummyResponseHeader =  array(
-			'HTTP/1.1 301 Moved Permanently',
-			'Server: nginx',
-			'Date: Fri, 25 Apr 2014 08:26:15 GMT',
-			'Content-Type: text/html',
-			'Content-Length: 11',
-			'Connection: close'
-		);
-		$dummyServerResponse = array_merge($dummyResponseHeader, array('', $dummyContent));
-
-		$fp = fopen('php://memory', 'rw');
-		fwrite($fp, implode("\n", $dummyServerResponse));
-		rewind($fp);
-
-		$crawlerLibrary = new tx_crawler_lib_proxy();
-		$response = $crawlerLibrary->getHttpResponseFromStream($fp);
-
-		$this->assertCount(6, $response['headers']);
-		$this->assertEquals($dummyResponseHeader, $response['headers']);
-		$this->assertEquals($dummyContent, $response['content'][0]);
 	}
 }
