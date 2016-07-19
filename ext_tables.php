fd2c9e04ea87e97bc3f70b9e01cae6658cf7ae97
Stefan Rotsch <stefan.rotsch@aoe.com>
2016-07-19 15:53:44 +0200

    [FEATURE] Introduce backend utility class to clean up ext_tables.php

--- ext_tables.php
+++ ext_tables.php
@@ -1,20 +1,11 @@
 <?php
-if (!defined ('TYPO3_MODE')) die ('Access denied.');
 
-if (TYPO3_MODE === 'BE') {
-	// add info module function
-	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::insertModuleFunction(
-		'web_info',
-		'tx_crawler_modfunc1',
-		NULL,
-		'LLL:EXT:crawler/locallang_db.php:moduleFunction.tx_crawler_modfunc1'
-	);
-
-	// add context menu item
-	$GLOBALS['TBE_MODULES_EXT']['xMOD_alt_clickmenu']['extendCMclasses'][] = array(
-		'name' => 'tx_crawler_contextMenu',
-		'path' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'class.tx_crawler_contextMenu.php'
-	);
+if (!defined('TYPO3_MODE')) {
+    die ('Access denied.');
 }
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_crawler_configuration');
+if ('BE' === TYPO3_MODE) {
+    \AOE\Crawler\Utility\BackendUtility::registerInfoModuleFunction();
+    \AOE\Crawler\Utility\BackendUtility::registerClickMenuItem();
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_crawler_configuration');
+}
