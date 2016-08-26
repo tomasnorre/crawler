270214640163a83b58add6e37b63d8f76e601070
Stefan Rotsch <stefan.rotsch@aoe.com>
2016-08-26 11:21:53 +0200

    [FEATURE] Adjust autoload configuration for TYPO3 7+
    Additionally, clean up ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,46 +1,22 @@
 <?php
-
-########################################################################
-# Extension Manager/Repository config file for ext "crawler".
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
-
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'Site Crawler',
-	'description' => 'Libraries and scripts for crawling the TYPO3 page tree. Used for re-caching, re-indexing, publishing applications etc.',
-	'category' => 'module',
-	'shy' => 0,
-	'version' => '5.0.7-dev',
-	'dependencies' => '',
-	'conflicts' => '',
-	'priority' => '',
-	'loadOrder' => '',
-	'module' => '',
-	'state' => 'stable',
-	'uploadfolder' => 0,
-	'createDirs' => '',
-	'modify_tables' => '',
-	'clearcacheonload' => 0,
-	'lockType' => '',
-	'author' => 'Kasper Skaarhoj, Daniel Poetzinger, Fabrizio Branca, Tolleiv Nietsch, Timo Schmidt, Michael Klapper, Stefan Rotsch, Tomas Norre Mikkelsen',
-	'author_email' => 'dev@aoe.com',
-	'author_company' => 'AOE GmbH',
-	'CGLcompliance' => '',
-	'CGLcompliance_note' => '',
-	'constraints' => array(
-		'depends' => array(
-			'php' => '5.3.0-7.99.99',
-			'typo3' => '6.2.0-7.99.99',
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		),
-	),
-	'_md5_values_when_last_written' => '',
-	'suggests' => array(
-	),
+    'title' => 'Site Crawler',
+    'description' => 'Libraries and scripts for crawling the TYPO3 page tree.',
+    'category' => 'module',
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'author' => 'Kasper Skaarhoj, Daniel Poetzinger, Fabrizio Branca, Tolleiv Nietsch, Timo Schmidt, Michael Klapper, Stefan Rotsch, Tomas Norre Mikkelsen',
+    'author_email' => 'dev@aoe.com',
+    'author_company' => 'AOE GmbH',
+    'version' => '5.0.7-dev',
+    'constraints' => array(
+        'depends' => array(
+            'php' => '5.3.0-7.99.99',
+            'typo3' => '6.2.0-7.99.99',
+        ),
+        'conflicts' => array(),
+        'suggests' => array(),
+    )
 );
