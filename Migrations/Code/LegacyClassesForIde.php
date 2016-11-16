eb4a1251eeb1ae08530bc7bd74a13adf00ca15f5
Tomas Norre Mikkelsen <tomas.mikkelsen@aoe.com>
2016-11-16 20:22:18 +0100

    [BUGFIX] Scheduler Tasks broken after upgrade to 5.1.0 (#142)

---
+++ Migrations/Code/LegacyClassesForIde.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @deprecated
+ */
+class tx_crawler_scheduler_flush extends AOE\Crawler\Task\FlushQueueTask {}
+
+/**
+ * @deprecated
+ */
+class tx_crawler_scheduler_im extends AOE\Crawler\Task\CrawlerQueueTask {}
+
+/**
+ * @deprecated
+ */
+class tx_crawler_scheduler_crawlMultiProcess extends AOE\Crawler\Task\CrawlMultiProcessTask {}
+
+/**
+ * @deprecated
+ */
+class tx_crawler_scheduler_crawl extends AOE\Crawler\Task\CrawlerTask {}
