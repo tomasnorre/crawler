d2c7b5d346f2cf3bb20445cc054fb048fe02ef55
Robert Vock <rvock@users.noreply.github.com>
2016-11-23 15:28:29 +0100

    [BUGFIX] Fixed incorrect class name in ClassAliasMap (#153)

--- Migrations/Code/ClassAliasMap.php
+++ Migrations/Code/ClassAliasMap.php
@@ -3,5 +3,5 @@ return array(
 	'tx_crawler_scheduler_flush' => 'AOE\\Crawler\\Task\\FlushQueueTask',
 	'tx_crawler_scheduler_im' => 'AOE\\Crawler\\Task\\CrawlerQueueTask',
 	'tx_crawler_scheduler_crawlMultiProcess' => 'AOE\\Crawler\\Task\\CrawlMultiProcessTask',
-	'tx_crawler_scheduler_crawl' => 'AOE\\Crawler\\Tasks\\CrawlerTask'
+	'tx_crawler_scheduler_crawl' => 'AOE\\Crawler\\Task\\CrawlerTask'
 );
