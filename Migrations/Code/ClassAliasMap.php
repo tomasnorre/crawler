6382907ffd3a358a10cccf7cbe0e407717a8ff8b
Tomas Norre Mikkelsen <tomas.mikkelsen@aoe.com>
2016-11-23 18:50:05 +0100

    [BUGFIX] Scheduler Tasks broken after upgrade to 5.1.1 (composer) (#154)

--- Migrations/Code/ClassAliasMap.php
+++ Migrations/Code/ClassAliasMap.php
@@ -1,7 +1,8 @@
 <?php
 return array(
-	'tx_crawler_scheduler_flush' => 'AOE\\Crawler\\Task\\FlushQueueTask',
-	'tx_crawler_scheduler_im' => 'AOE\\Crawler\\Task\\CrawlerQueueTask',
-	'tx_crawler_scheduler_crawlMultiProcess' => 'AOE\\Crawler\\Task\\CrawlMultiProcessTask',
-	'tx_crawler_scheduler_crawl' => 'AOE\\Crawler\\Task\\CrawlerTask'
+    'tx_crawler_scheduler_flush' => 'AOE\\Crawler\\Task\\FlushQueueTask',
+    'tx_crawler_scheduler_im' => 'AOE\\Crawler\\Task\\CrawlerQueueTask',
+    'tx_crawler_scheduler_crawlMultiProcess' => 'AOE\\Crawler\\Task\\CrawlMultiProcessTask',
+    'tx_crawler_scheduler_crawl' => 'AOE\\Crawler\\Task\\CrawlerTask',
+    'AOE\Crawler\Tasks\ProcessCleanupTask' => 'AOE\\Crawler\\Task\\ProcessCleanupTask'
 );
