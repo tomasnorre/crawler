ad3f3467e362a4005de311799b5ca7c14b70d9cc
Stefan Rotsch <stefan.rotsch@aoe.com>
2016-12-06 16:26:35 +0100

    [FEATURE] Add TYPO3 8.x as a supported version

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = array(
     'constraints' => array(
         'depends' => array(
             'php' => '5.3.0-7.99.99',
-            'typo3' => '6.2.0-7.99.99',
+            'typo3' => '6.2.0-8.99.99',
         ),
         'conflicts' => array(),
         'suggests' => array(),
