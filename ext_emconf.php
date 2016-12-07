92f3b13c363af6fbe0db8c8306cd2a0d8b1c809e
Stefan Rotsch <stefan.rotsch@aoe.com>
2016-12-07 09:57:53 +0100

    [FEATURE] Bump minimum required PHP version to 5.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '5.2.0-dev',
     'constraints' => array(
         'depends' => array(
-            'php' => '5.3.0-7.99.99',
+            'php' => '5.5.0-7.99.99',
             'typo3' => '6.2.0-8.99.99',
         ),
         'conflicts' => array(),
