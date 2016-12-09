9864745a295a08a24b927eedb19eb259a37de429
Tomas Norre Mikkelsen <tomasnorre@gmail.com>
2016-12-09 20:59:00 +0100

    [CLEANUP] move tx_crawler_domain_lib_abstract_repository (#175)
    Resolves: #175

--- Migrations/Code/LegacyClassesForIde.php
+++ Migrations/Code/LegacyClassesForIde.php
@@ -8,6 +8,11 @@ class tx_crawler_auth extends AOE\Crawler\Service\AuthenticationService {}
 /**
  * @deprecated
  */
+class tx_crawler_domain_lib_abstract_repository extends AOE\Crawler\Domain\Repository\AbstractRepository {}
+
+/**
+ * @deprecated
+ */
 class tx_crawler_scheduler_flush extends AOE\Crawler\Task\FlushQueueTask {}
 
 /**
