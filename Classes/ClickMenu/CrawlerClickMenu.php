354f0c5d01252623ab7138517080bcfbaae8ebd8
Chetan Thapliyal <chetan.thapliyal@aoe.com>
2017-01-03 12:00:18 +0100

    [TASK] TYPO3 8 refactoring
    - Substitute deprecated method calls
    - Use new coding conventions

--- Classes/ClickMenu/CrawlerClickMenu.php
+++ Classes/ClickMenu/CrawlerClickMenu.php
@@ -1,29 +1,23 @@
 <?php
 namespace AOE\Crawler\ClickMenu;
 
-/***************************************************************
- *  Copyright notice
+/*
+ * Copyright notice
  *
- *  (c) 2016 AOE GmbH <dev@aoe.com>
+ * (c) 2016 AOE GmbH <dev@aoe.com>
  *
- *  All rights reserved
+ * All rights reserved
+ * This file is part of the TYPO3 CMS project.
  *
- *  This script is part of the TYPO3 project. The TYPO3 project is
- *  free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 3 of the License, or
- *  (at your option) any later version.
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
  *
- *  The GNU General Public License can be found at
- *  http://www.gnu.org/copyleft/gpl.html.
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
  *
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  This copyright notice MUST APPEAR in all copies of the script!
- ***************************************************************/
+ * The TYPO3 project - inspiring people to share!
+ */
 
 use TYPO3\CMS\Backend\ClickMenu\ClickMenu;
 use TYPO3\CMS\Backend\Utility\BackendUtility;
@@ -37,11 +31,10 @@ use TYPO3\CMS\Extbase\Utility\LocalizationUtility;
  */
 class CrawlerClickMenu
 {
-
     /**
      * Main function
      *
-     * @param ClickMenu reference parent object
+     * @param ClickMenu $backRef reference parent object
      * @param array $menuItems
      * @param string $tableName
      * @param integer $uid
@@ -91,7 +84,7 @@ class CrawlerClickMenu
     {
         $icon = sprintf(
             '<img src="%s" border="0" align="top" alt="" />',
-            ExtensionManagementUtility::extRelPath('crawler') . 'icon_tx_crawler_configuration.gif'
+            ExtensionManagementUtility::siteRelPath('crawler') . 'icon_tx_crawler_configuration.gif'
         );
 
         return $icon;
--- Classes/Utility/TcaUtility.php
+++ Classes/Utility/TcaUtility.php
@@ -1,29 +1,23 @@
 <?php
 namespace AOE\Crawler\Utility;
 
-/***************************************************************
- *  Copyright notice
+/*
+ * Copyright notice
  *
- *  (c) 2016 AOE GmbH <dev@aoe.com>
+ * (c) 2016 AOE GmbH <dev@aoe.com>
  *
- *  All rights reserved
+ * All rights reserved
+ * This file is part of the TYPO3 CMS project.
  *
- *  This script is part of the TYPO3 project. The TYPO3 project is
- *  free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 3 of the License, or
- *  (at your option) any later version.
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
  *
- *  The GNU General Public License can be found at
- *  http://www.gnu.org/copyleft/gpl.html.
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
  *
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  This copyright notice MUST APPEAR in all copies of the script!
- ***************************************************************/
+ * The TYPO3 project - inspiring people to share!
+ */
 
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
@@ -34,7 +28,6 @@ use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
  */
 class TcaUtility
 {
-
     /**
      * Get crawler processing instructions.
      * This function is called as a itemsProcFunc in tx_crawler_configuration.processing_instruction_filter
@@ -67,7 +60,7 @@ class TcaUtility
             $parts = explode('_', $key);
             if (is_array($parts) && count($parts) > 2) {
                 $extensionKey = ExtensionManagementUtility::getExtensionKeyByPrefix('tx_' . $parts[1]);
-                $extIcon = ExtensionManagementUtility::extRelPath($extensionKey) . 'ext_icon.gif';
+                $extIcon = ExtensionManagementUtility::siteRelPath($extensionKey) . 'ext_icon.gif';
             }
         }
 
