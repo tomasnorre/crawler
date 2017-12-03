7f61603ed9969857ddd3ae2f344a1d7a7be1e066
Tomas Norre Mikkelsen <tomasnorre@gmail.com>
2017-12-03 17:43:26 +0100

    [CLEANUP] Move Icon to Private/Resources/Icons (#235)

--- class.tx_crawler_tcaFunc.php
+++ class.tx_crawler_tcaFunc.php
@@ -1,20 +1,20 @@
 <?php
+
 /***************************************************************
  *  Copyright notice
  *
- *  Copyright (c) 2009, AOE media GmbH <dev@aoemedia.de>
+ *  (c) 2017 AOE GmbH <dev@aoe.com>
+ *
  *  All rights reserved
  *
  *  This script is part of the TYPO3 project. The TYPO3 project is
  *  free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
+ *  the Free Software Foundation; either version 3 of the License, or
  *  (at your option) any later version.
  *
  *  The GNU General Public License can be found at
  *  http://www.gnu.org/copyleft/gpl.html.
- *  A copy is found in the textfile GPL.txt and important notices to the license
- *  from the author is found in LICENSE.txt distributed with these scripts.
  *
  *  This script is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -25,15 +25,7 @@
  ***************************************************************/
 
 /**
- * Class with tca functions
- *
- * @author	Fabrizio Branca <fabrizio.branca@aoemedia.de>
- * @copyright Copyright (c) 2009, AOE media GmbH <dev@aoemedia.de>
- * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License
- * @version $Id: $
- * @since 2009-05-28
- * @package TYPO3
- * @subpackage crawler
+ * Class tx_crawler_tcaFunc
  */
 class tx_crawler_tcaFunc {
 
