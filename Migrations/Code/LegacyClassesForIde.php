2e3fd0d820b76f94f843460c29c92b227c39fc8c
Stefan Rotsch <stefan.rotsch@aoe.com>
2017-12-15 12:11:30 +0100

    [WIP][TASK] Deprecate non-namespaced class names

--- Migrations/Code/LegacyClassesForIde.php
+++ Migrations/Code/LegacyClassesForIde.php
@@ -17,6 +17,62 @@ class tx_crawler_domain_lib_abstract_repository extends AOE\Crawler\Domain\Repos
 /**
  * @deprecated
  */
+class tx_crawler_domain_process extends AOE\Crawler\Domain\Model\Process
+{
+}
+
+/**
+ * @deprecated
+ */
+class tx_crawler_domain_process_repository extends AOE\Crawler\Domain\Repository\ProcessRepository
+{
+}
+
+/**
+ * @deprecated
+ */
+class tx_crawler_domain_queue_entry extends AOE\Crawler\Domain\Model\Queue
+{
+}
+
+/**
+ * @deprecated
+ */
+class tx_crawler_domain_queue_repository extends AOE\Crawler\Domain\Repository\QueueRepository
+{
+}
+
+/**
+ * @deprecated
+ */
+class tx_crawler_domain_reason extends AOE\Crawler\Domain\Model\Reason
+{
+}
+
+/**
+ * @deprecated
+ */
+class tx_crawler_hooks_processCleanUp extends AOE\Crawler\Hooks\ProcessCleanUpHook
+{
+}
+
+/**
+ * @deprecated
+ */
+class tx_crawler_hooks_staticFileCacheCreateUri extends AOE\Crawler\Hooks\StaticFileCacheCreateUriHook
+{
+}
+
+/**
+ * @deprecated
+ */
+class tx_crawler_hooks_tsfe extends AOE\Crawler\Hooks\TsfeHook
+{
+}
+
+/**
+ * @deprecated
+ */
 class tx_crawler_scheduler_flush extends AOE\Crawler\Task\FlushQueueTask
 {
 }
