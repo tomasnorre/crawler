fcf01c829e8b99fa8932053b3d5a2eadb747a193
Michael Stucki <michael@stucki.io>
2018-01-24 23:41:53 +0100

    [BUGFIX] Fix missing locallang labels for TYPO3 7.6

--- ext_tables.php
+++ ext_tables.php
@@ -6,4 +6,12 @@ if ('BE' === TYPO3_MODE) {
     \AOE\Crawler\Utility\BackendUtility::registerClickMenuItem();
     \AOE\Crawler\Utility\BackendUtility::registerContextSensitiveHelpForTcaFields();
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_crawler_configuration');
+
+    $isVersion7 = \TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger(TYPO3_version) < 8000000;
+    if ($isVersion7) {
+        $mappings = ['core', 'general'];
+        foreach ($mappings as $mapping) {
+            $GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['EXT:lang/Resources/Private/Language/locallang_' . $mapping . '.xlf'][] = 'EXT:lang/locallang_' . $mapping . '.xlf';
+        }
+    }
 }
