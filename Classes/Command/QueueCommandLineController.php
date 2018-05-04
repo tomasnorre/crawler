d8407552444e2b93c3194666a39794675c7dedf6
Tomas Norre Mikkelsen <tomasnorre@gmail.com>
2018-05-04 20:24:16 +0200

    [TASK] Adds @codeCoverageIgnore (#291)

--- Classes/Command/QueueCommandLineController.php
+++ Classes/Command/QueueCommandLineController.php
@@ -31,6 +31,7 @@ use TYPO3\CMS\Core\Controller\CommandLineController;
  * Class QueueCommandLineController
  *
  * @package AOE\Crawler\Command
+ * @codeCoverageIgnore
  */
 class QueueCommandLineController extends CommandLineController
 {
