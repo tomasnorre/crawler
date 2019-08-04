f05983266c7032d3dba0546574e654948e62ccbd
Tomas Norre Mikkelsen <tomas.mikkelsen@aoe.com>
2019-08-04 19:33:48 +0200

    [CGL] Add missing ;

--- Classes/Api/CrawlerApi.php
+++ Classes/Api/CrawlerApi.php
@@ -434,10 +434,11 @@ class CrawlerApi
      */
     private function getCrawlerProcInstructions(): array
     {
+
         $crawlerProcInstructions = [];
         if (!empty($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['crawler']['procInstructions'])) {
             foreach($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['crawler']['procInstructions'] as $configuration) {
-                $crawlerProcInstructions[$configuration['key']] = $configuration['value']
+                $crawlerProcInstructions[$configuration['key']] = $configuration['value'];
             }
         }
 
