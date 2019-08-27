285489764c580885b1280dd10e5b28ffd3723ef9
Michael Stucki <michael@stucki.io>
2019-08-27 11:43:25 +0200

    [TASK] Require TYPO3 8.7 or later

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '6.6.4',
     'constraints' => [
         'depends' => [
-            'typo3' => '7.6.0-8.7.99',
+            'typo3' => '8.7.0-8.7.99',
         ],
         'conflicts' => [],
         'suggests' => [],
