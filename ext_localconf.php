337a37f89c4b8d4f43fb6e2028e8d437f6b97503
Benni Mack <benni@typo3.org>
2019-09-05 07:41:44 +0200

    [!!!][TASK] Remove custom Authentication Service (#460)
    EXT:crawler ships with a authentication service class which is not in use right now, for crawling workspace pages,
    doing authentcation via a CLI user which does not exist anymore (FE auth from CLI users is not allowed anymore since TYPO3 v8 anyways).
    
    The AuthenticationService is therefore removed for the time being, as there is no use-case nor need found currently.

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,23 +5,4 @@ defined('TYPO3_MODE') or die();
 \AOE\Crawler\Utility\SchedulerUtility::registerSchedulerTasks($_EXTKEY);
 \AOE\Crawler\Utility\BackendUtility::registerIcons();
 
-$GLOBALS['TYPO3_CONF_VARS']['SVCONF']['auth']['setup']['BE_alwaysFetchUser'] = true;
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addService(
-    $_EXTKEY,
-    'auth',
-    'AOE\Crawler\Service\AuthenticationService',
-    [
-        'title' => 'Login for wsPreview',
-        'description' => '',
-        'subtype' => 'getUserBE,authUserBE',
-        'available' => true,
-        'priority' => 80,
-        'quality' => 50,
-        'os' => '',
-        'exec' => '',
-        'className' => \AOE\Crawler\Service\AuthenticationService::class,
-    ]
-);
-
 $GLOBALS['TYPO3_CONF_VARS']['BE']['ContextMenu']['ItemProviders'][1566472321] = \AOE\Crawler\ContextMenu\ItemProvider::class;
