2c62e0b768900c0c24075571d35964b75bf38b3c
Tomas Norre Mikkelsen <tomasnorre@gmail.com>
2020-05-08 17:19:24 +0200

    [TASK] Update License in classes where contributers already agreed

--- Classes/Utility/TcaUtility.php
+++ Classes/Utility/TcaUtility.php
@@ -5,12 +5,9 @@ declare(strict_types=1);
 namespace AOE\Crawler\Utility;
 
 /*
- * Copyright notice
+ * (c) 2020 AOE GmbH <dev@aoe.com>
  *
- * (c) 2018 AOE GmbH <dev@aoe.com>
- *
- * All rights reserved
- * This file is part of the TYPO3 CMS project.
+ * This file is part of the TYPO3 Crawler Extension.
  *
  * It is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License, either version 2
