b70c39cec179cee416334923c1b285ed68a094b9
Tomas Norre Mikkelsen <tomas.mikkelsen@aoe.com>
2020-07-18 21:36:11 +0200

    [GCL] Fix from ecs

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -1,12 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 defined('TYPO3_MODE') or die();
 
 $isVersion9 = \TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger(TYPO3_version) < 10000000;
-if (!$isVersion9) {
-    return[];
+if (! $isVersion9) {
+    return [];
 }
-
 
 $tca = [
     'palettes' => [
