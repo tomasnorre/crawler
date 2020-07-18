b55cb9614aab91ff6f960ebf7eb2a3f66d5b0dbe
Tomas Norre Mikkelsen <tomas.mikkelsen@aoe.com>
2020-07-18 20:32:19 +0200

    Fixing tests

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -3,8 +3,8 @@
 defined('TYPO3_MODE') or die();
 
 $isVersion9 = \TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger(TYPO3_version) < 10000000;
-if (!isVersion9) {
-    return;
+if (!$isVersion9) {
+    return[];
 }
 
 
