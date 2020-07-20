f55f07f56ab923bc3afc9cc4d4492d935a7513d9
Michael Wagner <rengaw83@users.noreply.github.com>
2020-07-20 12:43:24 +0200

    [BUGFIX] Load crawler initialization before tsfe rendering preparation (#610)

--- Configuration/RequestMiddlewares.php
+++ Configuration/RequestMiddlewares.php
@@ -22,7 +22,7 @@ return [
                 'typo3/cms-frontend/tsfe',
             ],
             'before' => [
-                'typo3/cms-frontend/output-compression',
+                'typo3/cms-frontend/prepare-tsfe-rendering',
             ],
         ],
     ],
