5d93e0837d360370e6720c404fa829e962a3b8e5
Tomas Norre Mikkelsen <tomas.mikkelsen@aoe.com>
2020-10-17 12:46:23 +0200

    [BUGFIX] Ensure that DataHandlerHook will not add pages to queue that does not exist

--- Tests/Unit/Hooks/DataHandlerHookTest.php
+++ Tests/Unit/Hooks/DataHandlerHookTest.php
@@ -29,6 +29,7 @@ use TYPO3\CMS\Core\Cache\Frontend\FrontendInterface;
 use TYPO3\CMS\Core\DataHandling\DataHandler;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Object\ObjectManager;
+use TYPO3\CMS\Frontend\Page\PageRepository;
 
 class DataHandlerHookTest extends UnitTestCase
 {
@@ -49,9 +50,14 @@ class DataHandlerHookTest extends UnitTestCase
         $queueRepository->isPageInQueue(1)->willReturn(false);
         $queueRepository->isPageInQueue(2)->willReturn(true);
 
+        $pageRepository = $this->prophesize(PageRepository::class);
+        $pageRepository->getPage(1)->willReturn(['Faking that page exists as not empty array']);
+        $pageRepository->getPage(2)->willReturn(['Faking that page exists as not empty array']);
+
         $objectManager = $this->prophesize(ObjectManager::class);
         $objectManager->get(QueueRepository::class)->willReturn($queueRepository->reveal());
         $objectManager->get(CrawlerApi::class)->willReturn($crawlerApi->reveal());
+        $objectManager->get(PageRepository::class)->willReturn($pageRepository->reveal());
 
         $cacheManager = $this->prophesize(CacheManager::class);
         $cacheManager->getCache(Argument::any())->willReturn($this->prophesize(FrontendInterface::class)->reveal());
@@ -89,9 +95,15 @@ class DataHandlerHookTest extends UnitTestCase
         $queueRepository->isPageInQueue(2)->willReturn(true);
         $queueRepository->isPageInQueue(3)->willReturn(false);
 
+        $pageRepository = $this->prophesize(PageRepository::class);
+        $pageRepository->getPage(1)->willReturn(['Faking that page exists as not empty array']);
+        $pageRepository->getPage(2)->willReturn(['Faking that page exists as not empty array']);
+        $pageRepository->getPage(3)->willReturn(['Faking that page exists as not empty array']);
+
         $objectManager = $this->prophesize(ObjectManager::class);
         $objectManager->get(QueueRepository::class)->willReturn($queueRepository->reveal());
         $objectManager->get(CrawlerApi::class)->willReturn($crawlerApi->reveal());
+        $objectManager->get(PageRepository::class)->willReturn($pageRepository->reveal());
 
         $cacheManager = $this->prophesize(CacheManager::class);
         $cacheManager->getCache(Argument::any())->willReturn($this->prophesize(FrontendInterface::class)->reveal());
@@ -105,6 +117,49 @@ class DataHandlerHookTest extends UnitTestCase
             [
                 'table' => 'tt_content',
                 'pageIdArray' => [0, 1, 2, 3],
+            ],
+            $dataHandler
+        );
+    }
+
+    /**
+     * Page with ID 1 is not in queue, should be added
+     * Page with ID 2 is already in queue. Should NOT be added.
+     * Page with ID 3 is not in queue, should be added
+     *
+     * @test
+     */
+    public function nothingToBeAddedAsPageDoNotExists(): void
+    {
+        $dataHandlerHook = new DataHandlerHook();
+        $crawlerApi = $this->prophesize(CrawlerApi::class);
+        $crawlerApi->addPageToQueue(1)->shouldBeCalled();
+
+        $queueRepository = $this->prophesize(QueueRepository::class);
+        $queueRepository->isPageInQueue(1)->willReturn(false);
+
+        $pageRepository = $this->prophesize(PageRepository::class);
+        $pageRepository->getPage(1)->willReturn(['Faking that page exists as not empty array']);
+        // Empty array to act like pages doesn't exist
+        $pageRepository->getPage(3000)->willReturn([]);
+
+        $objectManager = $this->prophesize(ObjectManager::class);
+        $objectManager->get(QueueRepository::class)->willReturn($queueRepository->reveal());
+        $objectManager->get(CrawlerApi::class)->willReturn($crawlerApi->reveal());
+        $objectManager->get(PageRepository::class)->willReturn($pageRepository->reveal());
+
+        $cacheManager = $this->prophesize(CacheManager::class);
+        $cacheManager->getCache(Argument::any())->willReturn($this->prophesize(FrontendInterface::class)->reveal());
+
+        GeneralUtility::setSingletonInstance(CacheManager::class, $cacheManager->reveal());
+        GeneralUtility::setSingletonInstance(ObjectManager::class, $objectManager->reveal());
+
+        $dataHandler = new DataHandler();
+
+        $dataHandlerHook->addFlushedPagesToCrawlerQueue(
+            [
+                'table' => 'tt_content',
+                'pageIdArray' => [0, 1, 3000],
             ],
             $dataHandler
         );
