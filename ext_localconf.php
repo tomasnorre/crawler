ed36259e9e0ba2e9d98e9f1aa9a15dd46c48331b
Tomas Norre Mikkelsen <tomas.mikkelsen@aoe.com>
2020-11-27 21:31:22 +0100

    [BUGFIX] Auto-loading for non-composer projects fixed

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,3 +5,7 @@ defined('TYPO3_MODE') or die();
 \AOE\Crawler\Utility\BackendUtility::registerIcons();
 
 $GLOBALS['TYPO3_CONF_VARS']['BE']['ContextMenu']['ItemProviders'][1566472321] = \AOE\Crawler\ContextMenu\ItemProvider::class;
+
+if (!\TYPO3\CMS\Core\Core\Environment::isComposerMode()) {
+    require \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('crawler') . '/Resources/Private/Php/Libraries/vendor/autoload.php';
+}
