1bc7c5c0d6573566e11853f01c3350cf44606f20
Tomas Norre Mikkelsen <tomas.mikkelsen@aoe.com>
2020-12-20 00:41:40 +0100

    [WIP] Testing PageRepositoryFactory v2

---
+++ Classes/Domain/Repository/PageRepositoryFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AOE\Crawler\Domain\Repository;
+
+/*
+ * (c) 2020 AOE GmbH <dev@aoe.com>
+ *
+ * This file is part of the TYPO3 Crawler Extension.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Utility\VersionNumberUtility;
+
+class PageRepositoryFactory
+{
+    public static function create()
+    {
+        $isVersion9 = VersionNumberUtility::convertVersionNumberToInteger(TYPO3_version) < 10000000;
+        if ($isVersion9) {
+            return GeneralUtility::makeInstance(\TYPO3\CMS\Frontend\Repository\PageRepository::class);
+
+        }
+        return GeneralUtility::makeInstance(\TYPO3\CMS\Core\Domain\Repository\PageRepository::class);
+    }
+}
