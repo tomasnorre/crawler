ae6a0693485277f478e595d4f35e3e194e508470
Tomas Norre Mikkelsen <tomas.mikkelsen@aoe.com>
2020-12-30 10:03:51 +0100

    [TASK] Adding more tests

---
+++ Tests/Unit/CrawlStrategy/GuzzleExecutionStrategyTest.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AOE\Crawler\Tests\Unit\CrawlStrategy;
+
+/*
+ * (c) 2020 AOE GmbH <dev@aoe.com>
+ *
+ * This file is part of the TYPO3 Crawler Extension.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use AOE\Crawler\CrawlStrategy\GuzzleExecutionStrategy;
+use Nimut\TestingFramework\TestCase\UnitTestCase;
+use TYPO3\CMS\Core\Http\Response;
+use TYPO3\CMS\Core\Http\Stream;
+use TYPO3\CMS\Core\Http\Uri;
+
+class GuzzleExecutionStrategyTest extends UnitTestCase
+{
+    /**
+     * @var GuzzleExecutionStrategy
+     */
+    protected $guzzleExecutionStrategy;
+
+    protected function setUp(): void
+    {
+        $this->guzzleExecutionStrategy = $this->createPartialMock(
+            GuzzleExecutionStrategy::class,
+            ['getResponse']
+        );
+
+        $body = $this->createPartialMock(Stream::class, ['getContents']);
+        $body->expects($this->any())
+            ->method('getContents')
+            ->willReturn(serialize('Crawler extension for TYPO3'));
+
+        $response = $this->createPartialMock(Response::class, ['getBody']);
+        $response->expects($this->any())
+            ->method('getBody')
+            ->willReturn($body);
+
+        $this->guzzleExecutionStrategy
+            ->expects($this->any())
+            ->method('getResponse')
+            ->willReturn($response);
+    }
+
+    /**
+     * @test
+     */
+    public function fetchUrlContents(): void
+    {
+        $crawlerId = sha1('this-is-testing');
+        $url = new Uri('https://not-important.tld');
+
+        self::assertStringContainsString(
+            'Crawler extension for TYPO3',
+            $this->guzzleExecutionStrategy->fetchUrlContents($url, $crawlerId)
+        );
+    }
+}
