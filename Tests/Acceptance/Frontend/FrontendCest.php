1be98837656182d25ff334551ea71d572247f0cb
Tomas Norre Mikkelsen <tomasnorre@gmail.com>
2021-10-29 10:35:16 +0200

    Update license

--- Tests/Acceptance/Frontend/FrontendCest.php
+++ Tests/Acceptance/Frontend/FrontendCest.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace AOE\Crawler\Tests\Acceptance\BackendModule;
 
 /*
- * (c) 2021 AOE GmbH <dev@aoe.com>
+ * (c) 2021 Tomas Norre Mikkelsen <tomasnorre@gmail.com>
  *
  * This file is part of the TYPO3 Crawler Extension.
  *
