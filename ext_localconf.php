751a5fa6cea2b36aff15e429bfac6547a7efdb9f
Tomas Norre Mikkelsen <tomasnorre@gmail.com>
2022-10-28 11:57:22 +0200

    [BUGFIX] If indexed_search is installed activate procInstructions for indexed_search
    Resolves: #960

--- ext_localconf.php
+++ ext_localconf.php
@@ -8,3 +8,15 @@ $GLOBALS['TYPO3_CONF_VARS']['BE']['ContextMenu']['ItemProviders'][1566472321] = 
 if (!\TYPO3\CMS\Core\Core\Environment::isComposerMode()) {
     require \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('crawler') . '/Resources/Private/Php/Libraries/vendor/autoload.php';
 }
+
+$packageManager = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Package\PackageManager::class);
+
+if ($packageManager->isPackageActive('indexed_search')) {
+// Register with "indexed_search" extension
+    $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['crawler']['procInstructions']['indexed_search'] = [
+        'key' => 'tx_indexedsearch_reindex',
+        'value' => 'Re-indexing'
+    ];
+}
+
+
