4597b3d69848aa4831d8cf378248d5876feb631d
Tomas Norre Mikkelsen <tomasnorre@gmail.com>
2022-11-04 07:28:04 +0100

    [BUGFIX] If indexed_search is installed activate procInstructions for indexed_search

--- ext_localconf.php
+++ ext_localconf.php
@@ -9,3 +9,16 @@ $GLOBALS['TYPO3_CONF_VARS']['BE']['ContextMenu']['ItemProviders'][1566472321] = 
 if (!\TYPO3\CMS\Core\Core\Environment::isComposerMode()) {
     require \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('crawler') . '/Resources/Private/Php/Libraries/vendor/autoload.php';
 }
+
+# This is done to ensure that ProcInstructions are loaded correctly under TYPO3 11, as the CrawlerHooks etc is
+# removed in Indexed Search in version 11.
+$packageManager = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Package\PackageManager::class);
+$typo3Version = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(TYPO3\CMS\Core\Information\Typo3Version::class);
+
+if ($packageManager->isPackageActive('indexed_search') && $typo3Version->getMajorVersion() === 11) {
+// Register with "indexed_search" extension
+    $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['crawler']['procInstructions']['indexed_search'] = [
+        'key' => 'tx_indexedsearch_reindex',
+        'value' => 'Re-indexing'
+    ];
+}
