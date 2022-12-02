1557d13d1e347f53122a60fbf8e8d1dcb6559777
Tomas Norre Mikkelsen <tomasnorre@gmail.com>
2022-12-02 10:36:25 +0100

    WIP: Backend module

--- Classes/Controller/BackendModuleController.php
+++ Classes/Controller/BackendModuleController.php
@@ -24,15 +24,23 @@ use Psr\Http\Message\ServerRequestInterface;
 use TYPO3\CMS\Backend\Template\Components\ButtonBar;
 use TYPO3\CMS\Backend\Template\ModuleTemplate;
 use TYPO3\CMS\Backend\Template\ModuleTemplateFactory;
+use TYPO3\CMS\Backend\Utility\BackendUtility;
+use TYPO3\CMS\Core\Authentication\BackendUserAuthentication;
 use TYPO3\CMS\Core\Imaging\Icon;
 use TYPO3\CMS\Core\Imaging\IconFactory;
+use TYPO3\CMS\Core\Localization\LanguageService;
+use TYPO3\CMS\Core\Type\Bitmask\Permission;
+use TYPO3\CMS\Extbase\Mvc\Web\Routing\UriBuilder;
 
 class BackendModuleController
 {
+    private $pageUid;
+
 
     public function __construct(
         protected ModuleTemplateFactory $moduleTemplateFactory,
-        protected IconFactory $iconFactory
+        protected IconFactory $iconFactory,
+        protected UriBuilder $uriBuilder
     )
     {
     }
@@ -41,8 +49,6 @@ class BackendModuleController
     {
         $moduleTemplate = $this->setupView($request);
         $moduleTemplate->assign('currentPageId', $request->getQueryParams()['id']);
-
-        $this->setUpDocHeader($request, $moduleTemplate);
         $moduleTemplate->setTitle('Crawler', $GLOBALS['LANG']->getLL('module.menu.log'));
 
         return $moduleTemplate->renderResponse('Backend/ShowLog');
@@ -54,20 +60,72 @@ class BackendModuleController
         $moduleTemplate->getView()->setLayoutRootPaths(['EXT:crawler/Resources/Private/Layouts']);
         $moduleTemplate->getView()->setPartialRootPaths(['EXT:crawler/Resources/Private/Partials']);
         $moduleTemplate->getView()->setTemplateRootPaths(['EXT:crawler/Resources/Private/Templates/Backend']);
+
+        list($moduleTemplate, $context) = $this->setupMenu($request, $moduleTemplate);
+
+        $moduleTemplate->setTitle(
+            $this->getLanguageService()->sL('LLL:EXT:examples/Resources/Private/Language/Module/locallang_mod.xlf:mlang_tabs_tab'),
+            $context
+        );
+
+        $permissionClause = $this->getBackendUserAuthentication()->getPagePermsClause(Permission::PAGE_SHOW);
+        $pageRecord = BackendUtility::readPageAccess(
+            $this->pageUid,
+            $permissionClause
+        );
+        if ($pageRecord) {
+            $moduleTemplate->getDocHeaderComponent()->setMetaInformation($pageRecord);
+        }
+        //$moduleTemplate->setFlashMessageQueue($this->getFlashMessageQueue());
+
         return $moduleTemplate;
     }
 
-    private function setUpDocHeader(
-        ServerRequestInterface $request,
-        ModuleTemplate         $view
-    )
+    public function setupMenu(ServerRequestInterface $request, ModuleTemplate $moduleTemplate): array
     {
-        $buttonBar = $view->getDocHeaderComponent()->getButtonBar();
-        $list = $buttonBar->makeLinkButton()
-            ->setHref('<uri-builder-path>')
-            ->setTitle('A Title')
-            ->setShowLabelText('Link')
-            ->setIcon($this->iconFactory->getIcon('actions-extension-import', Icon::SIZE_SMALL));
-        $buttonBar->addButton($list, ButtonBar::BUTTON_POSITION_LEFT, 1);
+        $menuItems = [
+            'flash' => [
+                'controller' => 'Module',
+                'action' => 'flash',
+                'label' => 'one',
+            ],
+            'tree' => [
+                'controller' => 'Module',
+                'action' => 'tree',
+                'label' => 'two',
+            ]
+        ];
+        $menu = $moduleTemplate->getDocHeaderComponent()->getMenuRegistry()->makeMenu();
+        $menu->setIdentifier('ExampleModuleMenu');
+
+        $context = '';
+        foreach ($menuItems as $menuItemConfig) {
+            $isActive = false; //$request->getControllerActionName() === $menuItemConfig['action'];
+            $menuItem = $menu->makeMenuItem()
+                ->setTitle($menuItemConfig['label'])
+                ->setHref($this->uriBuilder->reset()->uriFor(
+                    $menuItemConfig['action'],
+                    [],
+                    $menuItemConfig['controller']
+                ))
+                ->setActive($isActive);
+            $menu->addMenuItem($menuItem);
+            if ($isActive) {
+                $context = $menuItemConfig['label'];
+            }
+        }
+
+        $moduleTemplate->getDocHeaderComponent()->getMenuRegistry()->addMenu($menu);
+        return array($moduleTemplate, $context);
+    }
+
+    protected function getLanguageService(): LanguageService
+    {
+        return $GLOBALS['LANG'];
+    }
+
+    protected function getBackendUserAuthentication(): BackendUserAuthentication
+    {
+        return $GLOBALS['BE_USER'];
     }
 }
