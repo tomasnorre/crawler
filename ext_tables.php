371e37514ca319161933a9b7d7a647c9d8389a56
Tomas Norre Mikkelsen <tomasnorre@gmail.com>
2022-12-14 21:39:44 +0100

    Register Module for TYPO3 11

--- ext_tables.php
+++ ext_tables.php
@@ -1,4 +1,27 @@
 <?php
+
+use AOE\Crawler\Controller\Backend\BackendModuleStartCrawlingController;
+use TYPO3\CMS\Core\Information\Typo3Version;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
 defined('TYPO3') or die();
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_crawler_configuration');
+ExtensionManagementUtility::allowTableOnStandardPages('tx_crawler_configuration');
+
+$typo3MajorVersion = (new Typo3Version())->getMajorVersion();
+if ($typo3MajorVersion <= 11) {
+    ExtensionManagementUtility::addModule(
+        'web',
+        'CrawlerStart',
+        'after:web_info',
+        '',
+        [
+            'routeTarget' => BackendModuleStartCrawlingController::class . '::handleRequest',
+            'access' => 'user',
+            'workspace' => 'online',
+            'name' => 'web_CrawlerStart',
+            'icon' => 'EXT:crawler/Resources/Public/Icons/Extension.svg',
+            'labels' => 'LLL:EXT:crawler/Resources/Private/Language/Backend.xlf',
+        ]
+    );
+}
