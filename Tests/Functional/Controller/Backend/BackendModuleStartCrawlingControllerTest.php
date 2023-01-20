26cd8bd0e226194ab4e0f043efc8b9c7737b2dde
Tomas Norre Mikkelsen <tomasnorre@gmail.com>
2023-01-20 09:52:12 +0100

    wip

--- Tests/Functional/Controller/Backend/BackendModuleStartCrawlingControllerTest.php
+++ Tests/Functional/Controller/Backend/BackendModuleStartCrawlingControllerTest.php
@@ -25,8 +25,11 @@ use AOE\Crawler\Tests\Functional\BackendRequestTestTrait;
 use Prophecy\PhpUnit\ProphecyTrait;
 use TYPO3\CMS\Backend\Routing\UriBuilder;
 use TYPO3\CMS\Core\Authentication\BackendUserAuthentication;
+use TYPO3\CMS\Core\Core\SystemEnvironmentBuilder;
+use TYPO3\CMS\Core\Http\ServerRequest;
 use TYPO3\CMS\Core\Http\Uri;
 use TYPO3\CMS\Core\Localization\LanguageService;
+use TYPO3\CMS\Core\Routing\Route;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
 
@@ -63,11 +66,31 @@ class BackendModuleStartCrawlingControllerTest extends FunctionalTestCase
         $mockedCrawlerLogUri->withScheme('https')->withHost('crawler.tld');
         $mockedBackendUriBuilder = $this->createPartialMock(UriBuilder::class, ['buildUriFromRoute']);
         $mockedBackendUriBuilder->expects($this->once())->method('buildUriFromRoute')->with('web_site_crawler_log', ['id' => -1])->willReturn($mockedCrawlerLogUri);
-
         $subject = GeneralUtility::makeInstance(BackendModuleStartCrawlingController::class, $mockedCrawlerController, $mockedBackendUriBuilder);
 
-        $response = $subject->handleRequest($GLOBALS['TYPO3_REQUEST']);
+        $request = (new ServerRequest('https://example.com/typo3/'))
+            ->withAttribute('applicationType', SystemEnvironmentBuilder::REQUESTTYPE_BE)
+            ->withAttribute('route', new Route('path', ['packageName' => 'tomasnorre/crawler']));
 
+        $GLOBALS['TYPO3_REQUEST'] = $request;
+
+        //dd($request);
+
+        /**
+         * Have a look in core tests. The "Request" object needs prober setup (which is normally handled by the middleware stack I think).
+        $request = (new ServerRequest('https://example.com/typo3/'))
+        ->withAttribute('applicationType', SystemEnvironmentBuilder::REQUESTTYPE_BE)
+        ->withAttribute('route', new Route('path', ['packageName' => 'typo3/cms-backend']));
+        // your needed stuff for the request object, params etc
+        $GLOBALS['TYPO3_REQUEST'] = $request;
+        $controller->handleRequest($request);
+        the request object needs to have the route attribute, with the packageName of the extension (composer package ame). Maybe add that to your
+         * request object you hand over to the controller request handle method (edited)
+        with the package name, default kicks in (extension default template files -> ext:<ext>/Resources/Private/(Templates/Partials/Layouts)/` ...
+         * would bet the missing route/packagename is the missing puzzle piece
+         */
+
+        $response = $subject->handleRequest($request);
         self::assertEquals('test', $response->getBody());
     }
 }
