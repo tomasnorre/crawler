75fe78d43fbdcfb2c0a71c5ac14a27fe623b0685
Tomas Norre Mikkelsen <tomasnorre@gmail.com>
2025-01-22 07:13:54 +0100

    ci: enabled process tests

--- Tests/Acceptance-Playwright/Tests/backend_module_process.spec.js
+++ Tests/Acceptance-Playwright/Tests/backend_module_process.spec.js
@@ -11,45 +11,45 @@ async function addQueueEntries(page, config, depth = '0') {
     await expect(page.locator('#nprogress')).toHaveCount(0);
 }
 
-test.skip('Can Flush all processes', async ({page}) => {
+test('Can Flush all processes', async ({page}) => {
     await helpers.loginBackend(page)
     await addQueueEntries(page, 'default', '99');
     await helpers.openCrawlerModuleCrawlerProcesses(page)
-    await page.locator('iframe[name="list_frame"]').contentFrame().getByRole('link', {name: 'Add process'}).click();
-    await page.locator('iframe[name="list_frame"]').contentFrame().getByRole('link', {name: 'Add process'}).click();
+    await page.locator('iframe[name="list_frame"]').contentFrame().getByRole('button', {name: 'Add process'}).click();
+    await page.locator('iframe[name="list_frame"]').contentFrame().getByRole('button', {name: 'Add process'}).click();
     await expect(page.locator('iframe[name="list_frame"]').contentFrame().locator('#processes tbody tr')).toHaveCount(2)
-    await page.locator('iframe[name="list_frame"]').contentFrame().getByRole('link', {name: 'Flush all processes'}).click();
+    await page.locator('iframe[name="list_frame"]').contentFrame().getByRole('button', {name: 'Flush all processes'}).click();
     await expect(page.locator('iframe[name="list_frame"]').contentFrame().locator('#processes tbody tr')).toHaveCount(0)
 });
 
-test.skip('Can disable and enable crawler', async ({page}) => {
+test('Can disable and enable crawler', async ({page}) => {
     await helpers.loginBackend(page)
     await helpers.openCrawlerModuleCrawlerProcesses(page);
-    await page.locator('iframe[name="list_frame"]').contentFrame().getByRole('link', {name: 'Stop all processes and'}).click();
+    await page.locator('iframe[name="list_frame"]').contentFrame().getByRole('button', {name: 'Stop all processes and'}).click();
     await expect(page.locator('iframe[name="list_frame"]').contentFrame().locator('body')).toContainText('Enable crawling');
-    await page.locator('iframe[name="list_frame"]').contentFrame().getByRole('link', {name: 'Enable crawling'}).click();
+    await page.locator('iframe[name="list_frame"]').contentFrame().getByRole('button', {name: 'Enable crawling'}).click();
     await expect(page.locator('iframe[name="list_frame"]').contentFrame().locator('body')).toContainText('Stop all processes and disable crawling');
 });
 
-test.skip('Can add process', async ({page}) => {
+test('Can add process', async ({page}) => {
     await helpers.loginBackend(page)
     await addQueueEntries(page, 'default')
     await expect(page.locator('iframe[name="list_frame"]').contentFrame().getByText('URLs submitted')).toContainText('1 URLs submitted');
     await helpers.openCrawlerModuleCrawlerProcesses(page);
     await expect(page.locator('iframe[name="list_frame"]').contentFrame().getByText('CLI-Path')).toContainText('CLI-Path');
-    await page.locator('iframe[name="list_frame"]').contentFrame().getByRole('link', {name: 'Add process'}).click();
+    await page.locator('iframe[name="list_frame"]').contentFrame().getByRole('button', {name: 'Add process'}).click();
     await expect(page.locator('iframe[name="list_frame"]').contentFrame().getByText('New process has been started')).toContainText('New process has been started');
 });
 
-test.skip('Process successful', async ({page}) => {
+test('Process successful', async ({page}) => {
     await helpers.loginBackend(page)
     await addQueueEntries(page, 'default')
     await expect(page.locator('iframe[name="list_frame"]').contentFrame().getByText('URLs submitted')).toContainText('1 URLs submitted');
     await helpers.openCrawlerModuleCrawlerProcesses(page);
     await expect(page.locator('iframe[name="list_frame"]').contentFrame().getByText('CLI-Path')).toContainText('CLI-Path');
-    await page.locator('iframe[name="list_frame"]').contentFrame().getByRole('link', {name: 'Add process'}).click();
+    await page.locator('iframe[name="list_frame"]').contentFrame().getByRole('button', {name: 'Add process'}).click();
     await expect(page.locator('iframe[name="list_frame"]').contentFrame().getByText('New process has been started')).toContainText('New process has been started');
 
-    await page.locator('iframe[name="list_frame"]').contentFrame().getByRole('link', {name: 'Show finished and terminated processes'}).click();
-    await expect(page.locator('iframe[name="list_frame"]').contentFrame().getByText('Process completed successfully')).toContainText('Process completed successfully');
+    await page.locator('iframe[name="list_frame"]').contentFrame().getByRole('button', {name: 'Show finished and terminated processes'}).click();
+    await expect(page.locator('iframe[name="list_frame"]').contentFrame().locator("#processes")).toContainText('Process completed successfully');
 });
