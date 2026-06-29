45f989b0bea586c912628a2d167815721ac36ed3
Gregor Hermens <gregor.hermens@a-mazing.de>
2026-06-29 13:12:49 +0200

    [BUGFIX] Mask dots in regex pattern for php version check

--- Build/Scripts/runTests.sh
+++ Build/Scripts/runTests.sh
@@ -281,7 +281,7 @@ while getopts "a:b:d:i:s:p:xy:nhu" OPT; do
             ;;
         p)
             PHP_VERSION=${OPTARG}
-            if ! [[ ${PHP_VERSION} =~ ^(8.2|8.3|8.4|8.5)$ ]]; then
+            if ! [[ ${PHP_VERSION} =~ ^(8\.2|8\.3|8\.4|8\.5)$ ]]; then
                 INVALID_OPTIONS+=("p ${OPTARG}")
             fi
             ;;
